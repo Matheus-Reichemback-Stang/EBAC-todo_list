4f96bd3f680d908826633af77534408e1dfda922
zkd <matheus.reichemback.stang@gmail.com>
2024-09-25 15:43:09 -0300

    feat: adicionado interação de adição de tarefa e navegação para a página home

--- src/containers/Formulario/index.tsx
+++ src/containers/Formulario/index.tsx
@@ -1,22 +1,81 @@
+import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { cadastrar } from '../../store/reducers/tarefas'
+
 import { MainContainer, Titulo, Campo, BotaoSalvar } from '../../styles'
-import { Form, Opcoes } from './styles'
+import { Form, Opcoes, Opcao } from './styles'
+
+import * as enums from '../../utils/enums/Tarefa'
+import Tarefa from '../../models/Tarefa'
 
 const Formulario = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const [titulo, setTitulo] = useState('')
+  const [descricao, setDescricao] = useState('')
+  const [prioridade, setPrioridade] = useState(enums.Prioridade.NORMAL)
+
+  // O FormEvent é um tipo do React para configurar o submit de um form
+  const cadastrarTarefa = (evento: FormEvent) => {
+    evento.preventDefault()
+    const tarefaParaAdicionar = new Tarefa(
+      234,
+      titulo,
+      descricao,
+      prioridade,
+      enums.Status.PENDENTE
+    )
+
+    dispatch(cadastrar(tarefaParaAdicionar))
+    // Faz o retorno automatico para a rota definida quando executado
+    navigate('/')
+  }
+
   return (
     <MainContainer>
       <Titulo>Nova Tarefa</Titulo>
-      <Form>
-        <Campo type="text" placeholder="Titulo" />
-        <Campo as="textarea" placeholder="Descrição da tarefa" />
+      <Form onSubmit={cadastrarTarefa}>
+        <Campo
+          value={titulo}
+          onChange={(evento) => setTitulo(evento.target.value)}
+          type="text"
+          placeholder="Titulo"
+        />
+        <Campo
+          value={descricao}
+          onChange={({ target }) => setDescricao(target.value)}
+          as="textarea"
+          placeholder="Descrição da tarefa"
+        />
         <Opcoes>
           <p>Prioridade:</p>
           {/* é preciso colocar o atributo name com o valor identico para não conseguir selecionar mais de um item */}
-          <input name="prioridade" type="radio" id="urgente" />{' '}
-          <label htmlFor="urgente">Urgente</label>
-          <input name="prioridade" type="radio" id="importante" />{' '}
-          <label htmlFor="importante">Importante</label>
-          <input name="prioridade" type="radio" id="normal" />{' '}
-          <label htmlFor="normal">Normal</label>
+          {/* <input
+            value={enums.Prioridade.URGENTE}
+            name="prioridade"
+            type="radio"
+            id="urgente"
+          />{' '}
+          <label htmlFor="urgente">Urgente</label> */}
+          {Object.values(enums.Prioridade).map((prioridade) => (
+            <Opcao key={prioridade}>
+              <input
+                value={prioridade}
+                onChange={(evento) =>
+                  setPrioridade(evento.target.value as enums.Prioridade)
+                }
+                name="prioridade"
+                type="radio"
+                id={prioridade}
+                // Só vai deixar marcada a opção que tiver o valor Normal
+                defaultChecked={prioridade === enums.Prioridade.NORMAL}
+              />{' '}
+              <label htmlFor={prioridade}>{prioridade}</label>
+            </Opcao>
+          ))}
         </Opcoes>
         <BotaoSalvar type="submit">Cadastrar</BotaoSalvar>
       </Form>
